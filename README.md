95487c45c887b297510fc74d105f46ea4735ba57
easternferdinan <ferdinandrestu03@gmail.com>
2023-06-06 15:20:35 +0700

    Fixing product filter and updating README

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 This RESTful web service designed to support the operations of a food store application. It serves as the backbone of a food store system, handling various functionalities related to managing food products, customer orders, and user authentication.
 
 This project use:
+
 - Node.js v18.16.0
 - Express
 - MongoDB
@@ -10,49 +11,55 @@ This project use:
 - Pug (template engine)
 
 ## Features
+
 - **Product Management**: The API allows for the creation, retrieval, update, and deletion of food products. It provides endpoints to add new products, view product details, update product information, and remove products from the inventory.
 - **Order Processing**: The API enables the management of customer orders. It provides endpoints to place new orders, retrieve order details, update order status, and track the progress of orders. It also calculates order totals.
 - **User Authentication**: The API supports user authentication to ensure secure access to protected endpoints. It integrates with industry-standard authentication mechanism, JSON Web Tokens (JWT), allowing users to authenticate and authorize their requests.
 - **Search and Filtering**: The API provides search and filtering capabilities to help users find specific products based on various criteria. It supports queries such as searching for products by name.
 
 ## Usage
+
 ### Authentication:
+
 - #### Register:
-	Send POST request to:
 
-	`/auth/login`
+  Send POST request to:
 
-	With request body:
+  `/auth/login`
 
-		{
-			full_name: user full name
-			email: userEmail@email.com,
-			password: userpassword,
-			role: user or admin (default: user)
-		}
+  With request body:
+
+      {
+      	full_name: user full name
+      	email: userEmail@email.com,
+      	password: userpassword,
+      	role: user or admin (default: user)
+      }
 
 - #### Login:
-	Send POST request to:
 
-	`/auth/login`
+  Send POST request to:
 
-	with request body:
+  `/auth/login`
 
-		{
-			email: userEmail@email.com,
-			password: userpassword
-		}
+  with request body:
 
-	and "authorization" request header:
+      {
+      	email: userEmail@email.com,
+      	password: userpassword
+      }
 
-	`Authorization: Bearer [userToken]`
+  and "authorization" request header:
+
+  `Authorization: Bearer [userToken]`
 
 - #### Logout
-	Send POST request to:
 
-	`/auth/logout`
+  Send POST request to:
 
-	with `Authorization: Bearer [userToken]` header.
+  `/auth/logout`
+
+  with `Authorization: Bearer [userToken]` header.
 
 ### Products and orders handling:
 
@@ -65,152 +72,168 @@ Guest (not-logged in) users will only able to view the products.
 Add, update, delete is **restricted** to user with **admin** role only.
 
 - ##### View Products
-	GET `/api/products` or with optional skip and limit for pagination and filters which can be passed as query.
 
-	Possible queries:
-	- skip=Number
-	- limit=Number
-	- search=String
-	- category=String
-	- tags[]=[String]
+  GET `/api/products` or with optional skip and limit for pagination and filters which can be passed as query.
+
+  Possible queries:
+
+  - skip=Number
+  - limit=Number
+  - search=String
+  - category=String
+  - tags[]=String
 
 - ##### Add Product
-	POST `/api/products` with body:
 
-		{
-			name: productName,
-			description: productDescription,
-			price: 9999,
-			image: image.jpg (file),
-			category: categoryName,
-			tags[]: tagName
-		}
+  POST `/api/products` with body:
 
-	Allowed image format:
-	- .png
-	- .jpg
-	- .jpeg
-	- .gif
+      {
+      	name: productName,
+      	description: productDescription,
+      	price: 9999,
+      	image: image.jpg (file),
+      	category: categoryName,
+      	tags[]: tagName
+      }
+
+  Allowed image format:
+
+  - .png
+  - .jpg
+  - .jpeg
+  - .gif
 
 - ##### Update Product
-	PUT `/api/products/[product-id]` with body that specifies which field(s) is going to be updated and it's new value. Example:
 
-		{
-			name: productName,
-			price: 9999,
-			image: newImage.png
-		}
+  PUT `/api/products/[product-id]` with body that specifies which field(s) is going to be updated and it's new value. Example:
+
+      {
+      	name: productName,
+      	price: 9999,
+      	image: newImage.png
+      }
 
 - ##### Delete Product
-	DELETE `/api/products/[product-id]`
+  DELETE `/api/products/[product-id]`
 
 #### Category
 
 All `/api/categories` endpoints is **restricted** to **admin** only
 
 - ##### View Categories
-	GET `/api/categories`
+
+  GET `/api/categories`
 
 - ##### Add Category
-	POST `/api/categories` with a body containing:
 
-		{
-			name: categoryName
-		}
+  POST `/api/categories` with a body containing:
+
+      {
+      	name: categoryName
+      }
 
 - ##### Update Category
-	PUT `/api/categories/[category-id]` with a body:
 
-		{
-			name: newCategoryName
-		}
+  PUT `/api/categories/[category-id]` with a body:
+
+      {
+      	name: newCategoryName
+      }
 
 - ##### Delete Category
-	DELETE `/api/categories/[category-id]`
+  DELETE `/api/categories/[category-id]`
 
 #### Tag
 
 All `/api/tags` endpoints is **restricted** to **admin** only
 
 - ##### View Tags
-	GET `/api/tags`
+
+  GET `/api/tags`
 
 - ##### Add Tag
-	POST `/api/tags` with a body containing:
 
-		{
-			name: tagName
-		}
+  POST `/api/tags` with a body containing:
+
+      {
+      	name: tagName
+      }
 
 - ##### Update Tag
-	PUT `/api/tags/[tag-id]` with a body:
-	
-		{
-			name: newTagName
-		}
+
+  PUT `/api/tags/[tag-id]` with a body:
+
+      {
+      	name: newTagName
+      }
 
 - ##### Delete Tag
-	DELETE `/api/tags/[tag-id]`
+  DELETE `/api/tags/[tag-id]`
 
 #### Delivery Address
 
-All logged in users have access to every delivery address endpoints. 
+All logged in users have access to every delivery address endpoints.
 
 **Field specific authorization** is in effect for update and delete operation. So users only authorized to update and delete what's belong to them.
 
 - ##### View delivery addresses
-	GET `/api/delivery-addresses`
+
+  GET `/api/delivery-addresses`
 
 - ##### Add delivery address
-	POST `/api/delivery-addresses` with body:
 
-		{
-			label: addressLabel (ex: Alamat Rumah),
-			kelurahan: kelurahanName,
-			kecamatan: kecamatanName,
-			kota: kotaOrKabupatenName,
-			provinsi: provinsiName,
-			detail: addressDetails (street, block, unit number)
-		}
+  POST `/api/delivery-addresses` with body:
+
+      {
+      	label: addressLabel (ex: Alamat Rumah),
+      	kelurahan: kelurahanName,
+      	kecamatan: kecamatanName,
+      	kota: kotaOrKabupatenName,
+      	provinsi: provinsiName,
+      	detail: addressDetails (street, block, unit number)
+      }
 
 - ##### Update delivery address
-	PUT `/api/delivery-addresses/[address-id]` with body:
 
-		{
-			label: newAddressLabel (ex: Alamat Rumah),
-			kelurahan: newKelurahanName,
-			kecamatan: newKecamatanName,
-			kota: newKotaOrKabupatenName,
-			provinsi: newProvinsiName,
-			detail: AddressDetails (street, block, unit number)
-		}
+  PUT `/api/delivery-addresses/[address-id]` with body:
+
+      {
+      	label: newAddressLabel (ex: Alamat Rumah),
+      	kelurahan: newKelurahanName,
+      	kecamatan: newKecamatanName,
+      	kota: newKotaOrKabupatenName,
+      	provinsi: newProvinsiName,
+      	detail: AddressDetails (street, block, unit number)
+      }
 
 - ##### Delete delivery address
-	DELETE `/api/delivery-addresses/[address-id]`
+  DELETE `/api/delivery-addresses/[address-id]`
 
 #### Cart
 
-*Subject to changes. It's possible a controller for updating cart is added in the future. So user can update their cart instead of creating new cart everytime.
+\*Subject to changes. It's possible a controller for updating cart is added in the future. So user can update their cart instead of creating new cart everytime.
 
 - ##### View cart
-	GET `/api/cart`
+
+  GET `/api/cart`
 
 - ##### Add cart
-	POST `/api/cart` with body: 
 
-		[
-			{
-				product: productId,
-				quantity: 9999
-			},
-			{
-				product: productId,
-				quantity: 9999
-			}
-		]
+  POST `/api/cart` with body:
+
+      [
+      	{
+      		product: productId,
+      		quantity: 9999
+      	},
+      	{
+      		product: productId,
+      		quantity: 9999
+      	}
+      ]
 
 - ##### Delete cart
-	DELETE `/api/cart/[cart-id]`
+  DELETE `/api/cart/[cart-id]`
 
 #### Order
 
@@ -219,29 +242,33 @@ Field specific authorization is in effect for delete operation.
 Admin is the only one who can do update on orders, and it's only for updating delivery_status.
 
 - ##### View orders
-	GET `/api/order`
+
+  GET `/api/order`
 
 - ##### Add order
-	POST `/api/order` with body:
 
-		{
-			delivery_address: addressId
-		}
+  POST `/api/order` with body:
+
+      {
+      	delivery_address: addressId
+      }
 
 - ##### Update order
-	PUT `/api/order/[order-id]` with body:
 
-		{
-			delivery_status:  deliveryStatus
-		}
+  PUT `/api/order/[order-id]` with body:
 
-	delivery_status enums:
-	- Menunggu pembayaran
-	- Diproses
-	- Dalam pengiriman
+      {
+      	delivery_status:  deliveryStatus
+      }
+
+  delivery_status enums:
+
+  - Menunggu pembayaran
+  - Diproses
+  - Dalam pengiriman
 
 - ##### Delete order
-	DELETE `/api/order/[order-id]`
+  DELETE `/api/order/[order-id]`
 
 #### Invoice
 
@@ -252,6 +279,7 @@ Invoice is rendered with pug using data from Order.
 Field specific authorization is in effect.
 
 - ##### View invoice
-	GET `/api/invoice/[order-id]`
 
-	Pay attention to the params. It's using order id. There is no invoice id.
+  GET `/api/invoice/[order-id]`
+
+  Pay attention to the params. It's using order id. There is no invoice id.
--- app/products/productsController.js
+++ app/products/productsController.js
@@ -8,59 +8,75 @@ const Categories = require("../categories/categoriesModel");
 const Tags = require("../tags/tagsModel");
 
 const index = async (req, res) => {
-  const { skip = 0, limit = 0, search = "", category = "", tags = [] } = req.query;
+  const {
+    skip = 0,
+    limit = 0,
+    search = "",
+    category = "",
+    tags = [],
+  } = req.query;
 
   let criteria = {};
 
-  try {    
+  try {
     if (search.length) {
-      criteria = {...criteria, name: {$regex: search, $options: "i"}};
+      criteria = { ...criteria, name: { $regex: search, $options: "i" } };
     }
 
     if (category.length) {
-      const categorySearch = await Categories.findOne({name: {$regex: category, $options: "i"}});
-      console.log(categorySearch);
+      const categorySearch = await Categories.findOne({
+        name: { $regex: category, $options: "i" },
+      });
       if (categorySearch) {
-        criteria = {...criteria, category: categorySearch._id};
+        criteria = { ...criteria, category: categorySearch._id };
       }
     }
-    
+
     if (tags.length) {
-      const tagsSearch = await Tags.find({name: {$in: tags}});
+      const tagsSearch = await Tags.find({ name: { $in: tags } });
       if (tagsSearch.length) {
-        criteria = {...criteria, tags: tagsSearch.map(tag => tag._id)};
+        criteria = {
+          ...criteria,
+          tags: { $in: tagsSearch.map((tag) => tag._id.toString()) },
+        };
+        console.log(criteria);
       }
     }
 
-    if (search || category || tags && !criteria) {
+    if (
+      (category && !criteria.category) ||
+      (tags.length > 0 && criteria.tags.length < 1)
+    ) {
       return res.json({
-        message: "Tidak ada produk yang cocok dengan filter"
-      })
+        message: "Tidak ada produk yang cocok dengan filter",
+      });
     }
 
-    const products = await Products
-    .find(criteria)
-    .skip(parseInt(skip))
-    .limit(parseInt(limit))
-    .populate("category")
-    .populate("tags");
+    const products = await Products.find(criteria)
+      .skip(parseInt(skip))
+      .limit(parseInt(limit))
+      .populate("category")
+      .populate("tags");
 
     const count = await Products.countDocuments(criteria);
-    
-    return res.json({products, count});
+
+    return res.json({ products, count });
   } catch (error) {
     return res.status(500).json(error);
   }
-}
+};
 
 const store = async (req, res) => {
-  let payload = req.body
+  let payload = req.body;
 
-  if (payload.category) { // Handling category field 
+  if (payload.category) {
+    // Handling category field
     try {
-      const category = await Categories.findOne({name: {$regex: payload.category, $options: "i"}});
+      const category = await Categories.findOne({
+        name: { $regex: payload.category, $options: "i" },
+      });
       if (category) {
-        payload = {...payload, category: category._id};
+        payload = { ...payload, category: category._id };
       } else {
         delete payload.category;
       }
@@ -70,11 +86,14 @@ const store = async (req, res) => {
     }
   }
 
-  if (payload.tags && payload.tags.length > 0) { // Handling tags field
+  if (payload.tags && payload.tags.length > 0) {
+    // Handling tags field
     try {
-      const tags = await Tags.find({name: {$regex: new RegExp(payload.tags.join('|'), 'i')}});
+      const tags = await Tags.find({
+        name: { $regex: new RegExp(payload.tags.join("|"), "i") },
+      });
       if (tags.length > 0) {
-        payload = {...payload, tags: tags.map(tag => tag._id)};
+        payload = { ...payload, tags: tags.map((tag) => tag._id) };
       } else {
         delete payload.tags;
       }
@@ -84,35 +103,42 @@ const store = async (req, res) => {
     }
   }
 
-  if (req.file) { // Processing request with image
+  if (req.file) {
+    // Processing request with image
     const tempPath = req.file.path;
     const fileName = req.file.filename;
 
     // Splitting the file extension from the rest of the original name and then validating it
     // whether it's a jpg, jpeg, png, or gif.
-    const fileExtension = req.file.originalname.split(".")[req.file.originalname.split(".").length - 1];
+    const fileExtension =
+      req.file.originalname.split(".")[
+        req.file.originalname.split(".").length - 1
+      ];
     const extensionIsValid = /(jpg|jpeg|png|gif)$/i.test(fileExtension);
     if (!extensionIsValid) {
       return res.status(415).json({
         code: 415,
         error: "Unsupported Media Type",
-        message: "Mohon gunakan format gambar yang didukung (jpg, jpeg, png, gif)",
+        message:
+          "Mohon gunakan format gambar yang didukung (jpg, jpeg, png, gif)",
         providedImageExtension: fileExtension,
-      })
+      });
     }
 
     try {
       const uploadedImage = await cloudinary.uploader.upload(tempPath, {
         public_id: fileName,
-        folder: "/products-images/"
+        folder: "/products-images/",
       });
 
-      const products = new Products({...payload, image_url: uploadedImage.secure_url});
+      const products = new Products({
+        ...payload,
+        image_url: uploadedImage.secure_url,
+      });
       await products.save();
       return res.status(201).json(products);
-      
     } catch (error) {
-      if (error && error.name === "ValidationError"){
+      if (error && error.name === "ValidationError") {
         return res.status(422).json({
           code: 422,
           error: "Unprocessable entity",
@@ -123,35 +149,38 @@ const store = async (req, res) => {
         return res.status(500).json(error);
       }
     }
-
-  } else { // Processing request without image
+  } else {
+    // Processing request without image
     try {
       const products = new Products(payload);
       await products.save();
       return res.status(201).json(products);
     } catch (error) {
-      if (error && error.name === "ValidationError"){
+      if (error && error.name === "ValidationError") {
         return res.status(422).json({
           code: 422,
           error: "Unprocessable entity",
           message: error.message,
           detail: error.errors,
-        })
+        });
       } else {
         return res.status(500).json(error);
       }
     }
   }
-}
+};
 
 const update = async (req, res) => {
-  let payload = req.body
+  let payload = req.body;
 
-  if (payload.category) { // Handling category field
+  if (payload.category) {
+    // Handling category field
     try {
-      const category = await Categories.findOne({name: {$regex: payload.category, $options: "i"}});
+      const category = await Categories.findOne({
+        name: { $regex: payload.category, $options: "i" },
+      });
       if (category) {
-        payload = {...payload, category: category._id};
+        payload = { ...payload, category: category._id };
       } else {
         delete payload.category;
       }
@@ -161,11 +190,14 @@ const update = async (req, res) => {
     }
   }
 
-  if (payload.tags && payload.tags.length > 0) { // Handling tags field
+  if (payload.tags && payload.tags.length > 0) {
+    // Handling tags field
     try {
-      const tags = await Tags.find({name: {$regex: new RegExp(payload.tags.join('|'), 'i')}});
+      const tags = await Tags.find({
+        name: { $regex: new RegExp(payload.tags.join("|"), "i") },
+      });
       if (tags.length > 0) {
-        payload = {...payload, tags: tags.map(tag => tag._id)};
+        payload = { ...payload, tags: tags.map((tag) => tag._id) };
       } else {
         delete payload.tags;
       }
@@ -175,101 +207,118 @@ const update = async (req, res) => {
     }
   }
 
-  if (req.file) { // Processing request with image
+  if (req.file) {
+    // Processing request with image
     const tempPath = req.file.path;
     const fileName = req.file.filename;
 
     // Splitting the file extension from the rest of the original name and then validating it
     // whether it's a jpg, jpeg, png, or gif.
-    const fileExtension = req.file.originalname.split(".")[req.file.originalname.split(".").length - 1];
+    const fileExtension =
+      req.file.originalname.split(".")[
+        req.file.originalname.split(".").length - 1
+      ];
     const extensionIsValid = /(jpg|jpeg|png|gif)$/i.test(fileExtension);
     if (!extensionIsValid) {
       return res.status(415).json({
         code: 415,
         error: "Unsupported Media Type",
-        message: "Mohon gunakan format gambar yang didukung (jpg, jpeg, png, gif)",
+        message:
+          "Mohon gunakan format gambar yang didukung (jpg, jpeg, png, gif)",
         providedImageExtension: fileExtension,
-      })
+      });
     }
 
     try {
       const existingProduct = await Products.findById(req.params.id);
 
-      if (existingProduct.image_url) { // Removing existing image on cloud if exist
-        const imagePublicId = existingProduct.image_url.match(/(products-images\/[^\/.]+)/)[1];
+      if (existingProduct.image_url) {
+        // Removing existing image on cloud if exist
+        const imagePublicId = existingProduct.image_url.match(
+          /(products-images\/[^\/.]+)/
+        )[1];
         await cloudinary.uploader.destroy(imagePublicId, {
-          invalidate: true
+          invalidate: true,
         });
       }
 
       const newImage = await cloudinary.uploader.upload(tempPath, {
         public_id: fileName,
-        folder: "/products-images/"
+        folder: "/products-images/",
       });
 
-      const updatedProducts = await Products.findByIdAndUpdate(req.params.id, { ...payload, image_url: newImage.secure_url}, {
-        new: true,
-        runValidators: true,
-      });
+      const updatedProducts = await Products.findByIdAndUpdate(
+        req.params.id,
+        { ...payload, image_url: newImage.secure_url },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
 
       return res.status(201).json(updatedProducts);
-
     } catch (error) {
-      if (error && error.name === "ValidationError"){
+      if (error && error.name === "ValidationError") {
         return res.status(422).json({
           code: 422,
           error: "Unprocessable entity",
           message: error.message,
           detail: error.errors,
-        })
+        });
       } else {
         return res.status(500).json(error);
       }
     }
-
-  } else { // Processing request without image
+  } else {
+    // Processing request without image
     try {
-      const products = await Products.findByIdAndUpdate(req.params.id, payload, {
-        new: true,
-        runValidators: true,
-      });
+      const products = await Products.findByIdAndUpdate(
+        req.params.id,
+        payload,
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
       return res.status(201).json(products);
     } catch (error) {
-      if (error && error.name === "ValidationError"){
+      if (error && error.name === "ValidationError") {
         return res.status(422).json({
           code: 422,
           error: "Unprocessable entity",
           message: error.message,
           detail: error.errors,
-        })
+        });
       } else {
         return res.status(500).json(error);
       }
     }
   }
-}
+};
 
 const destroy = async (req, res) => {
   try {
     const existingProduct = await Products.findByIdAndDelete(req.params.id);
 
-    if (existingProduct.image_url) { // Removing existing image on cloud if exist
-      const imagePublicId = existingProduct.image_url.match(/(products-images\/[^\/.]+)/)[1];
+    if (existingProduct.image_url) {
+      // Removing existing image on cloud if exist
+      const imagePublicId = existingProduct.image_url.match(
+        /(products-images\/[^\/.]+)/
+      )[1];
       await cloudinary.uploader.destroy(imagePublicId, {
-        invalidate: true
+        invalidate: true,
       });
     }
 
     return res.json(existingProduct);
-
   } catch (error) {
     return res.status(500).json(error);
   }
-}
+};
 
 module.exports = {
   index,
   store,
   update,
   destroy,
-}
+};
